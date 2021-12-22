15711cc941ba755b285cdcc708f0f6c506284cb5
Paul <paul@theapplegates.com>
2021-12-22 10:22:57 -0500

    Update image.js

--- 11ty/shortcodes/image.js
+++ 11ty/shortcodes/image.js
@@ -5,7 +5,7 @@ const dir = require('../constants/dir');
 /** Returns optimized image markup. Expects to receive a source-relative image path (e.g., /assets/images/image.png).
  * Example usage: `{% image '/assets/images/image.png', 'Alt text' %}`.
  */
-async function imageShortcode(src, alt = '', widths = [300, 600], formats = ['avif', 'jpeg'], sizes = '100vw') {
+async function imageShortcode(src, alt = '', widths = [150, 300, 600, 1200], formats = ['avif', 'jpeg'], sizes = '100vw') {
   const { dir: imgDir } = path.parse(src);
   const fullyQualifiedSrc = path.join(dir.input, src);
 
